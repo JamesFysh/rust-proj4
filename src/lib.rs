29441009179b2401768440c10ee626f85e25957a
James Fysh <james.fysh@gmail.com>
2014-12-04 08:33:22 +1100

    Numerous changes to the implementation, use of ffi module etc.

--- src/lib.rs
+++ src/lib.rs
@@ -6,137 +6,109 @@
 extern crate libc;
 
 use libc::{c_double, c_int};
-use ffi::*;
 
-mod ffi;
+pub mod ffi;
 
-pub struct Projection(projPJ);
+pub struct Projection(ffi::projPJ);
 
 impl Drop for Projection {
     fn drop(&mut self) {
         unsafe {
             let &Projection(pj) = self;
-            pj_free(pj);
+            ffi::pj_free(pj);
         }
     }
 }
 
-pub struct Context(projCtx);
+pub struct Context(ffi::projCtx);
 
 impl Drop for Context {
     fn drop(&mut self) {
         unsafe {
             let &Context(c) = self;
-            pj_ctx_free(c);
+            ffi::pj_ctx_free(c);
         }
     }
 }
 
 pub fn init_plus(definition: &str) -> Projection {
     unsafe {
-        Projection(pj_init_plus(definition.to_c_str().as_ptr()))
+        Projection(ffi::pj_init_plus(definition.to_c_str().as_ptr()))
     }
 }
 
 pub fn transform(srcdefn: &Projection, dstdefn: &Projection, x: &mut f64, y: &mut f64) -> i32 {
-    let &Projection(src) = srcdefn;
-    let &Projection(dst) = dstdefn;
-    unsafe {
-        let c_x = x as *mut c_double;
-        let c_y = y as *mut c_double;
-        let c_z = 0 as *mut c_double;
-        pj_transform(src, dst, 1, 1, c_x, c_y, c_z) as i32
-    }
+    transformv(srcdefn, dstdefn, &[x], &[y])
 }
 
-pub fn transformv(srcdefn: &Projection, dstdefn: &Projection, x: &mut [f64], y: &mut [f64]) -> i32 {
+pub fn transformv(srcdefn: &Projection, dstdefn: &Projection, x: &[&mut f64], y: &[&mut f64]) -> i32 {
     assert!(x.len() == y.len());
     let &Projection(src) = srcdefn;
     let &Projection(dst) = dstdefn;
+    let c_x = x.as_ptr() as *mut c_double;
+    let c_y = y.as_ptr() as *mut c_double;
+    let c_z = 0 as *mut c_double;
     unsafe {
-        let c_x = x.as_ptr() as *mut c_double;
-        let c_y = y.as_ptr() as *mut c_double;
-        let c_z = 0 as *mut c_double;
-        pj_transform(src, dst, x.len() as i64, 1, c_x, c_y, c_z) as i32
+        ffi::pj_transform(src, dst, x.len() as i64, 1, c_x, c_y, c_z) as i32
     }
 }
 
 pub fn get_def(proj: &Projection, opts: int) -> String {
+    let &Projection(pj) = proj;
     unsafe {
-        let &Projection(pj) = proj;
-        let allocated = pj_get_def(pj, opts as c_int);
+        let allocated = ffi::pj_get_def(pj, opts as c_int);
         let def = String::from_raw_buf(allocated as *const u8);
-        pj_dalloc(allocated);
+        ffi::pj_dalloc(allocated);
         return def;
     }
 }
 
 pub fn is_latlong(proj: &Projection) -> bool {
-    unsafe {
-        let &Projection(pj) = proj;
-        pj_is_latlong(pj) == 1
-    }
+    let &Projection(pj) = proj;
+    unsafe { ffi::pj_is_latlong(pj) == 1 }
 }
 
 pub fn is_geocent(proj: &Projection) -> bool {
-    unsafe {
-        let &Projection(pj) = proj;
-        pj_is_geocent(pj) == 1
-    }
+    let &Projection(pj) = proj;
+    unsafe { ffi::pj_is_geocent(pj) == 1 }
 }
 
 pub fn get_release() -> String {
-    unsafe {
-        String::from_raw_buf(pj_get_release() as *const u8)
-    }
+    unsafe { String::from_raw_buf(ffi::pj_get_release() as *const u8) }
 }
 
 pub fn get_default_ctx() -> Context {
-    unsafe {
-        Context(pj_get_default_ctx())
-    }
+    unsafe { Context(ffi::pj_get_default_ctx()) }
 }
 
 pub fn get_ctx(proj: &Projection) -> Context {
-    unsafe {
-        let &Projection(pj) = proj;
-        Context(pj_get_ctx(pj))
-    }
+    let &Projection(pj) = proj;
+    unsafe { Context(ffi::pj_get_ctx(pj)) }
 }
 
 pub fn set_ctx(proj: &Projection, ctx: &Context) {
-    unsafe {
-        let &Projection(pj) = proj;
-        let &Context(ct) = ctx;
-        pj_set_ctx(pj, ct)
-    }
+    let &Projection(pj) = proj;
+    let &Context(ct) = ctx;
+    unsafe { ffi::pj_set_ctx(pj, ct) }
 }
 
 pub fn ctx_alloc() -> Context {
-    unsafe {
-        Context(pj_ctx_alloc())
-    }
+    unsafe { Context(ffi::pj_ctx_alloc()) }
 }
 
 pub fn ctx_get_errno(ctx: &Context) -> i32 {
-    unsafe {
-        let &Context(ct) = ctx;
-        pj_ctx_get_errno(ct) as i32
-    }
+    let &Context(ct) = ctx;
+    unsafe { ffi::pj_ctx_get_errno(ct) as i32 }
 }
 
 pub fn ctx_set_errno(ctx: &Context, errno: i32) {
-    unsafe {
-        let &Context(ct) = ctx;
-        pj_ctx_set_errno(ct, errno as c_int)
-    }
+    let &Context(ct) = ctx;
+    unsafe { ffi::pj_ctx_set_errno(ct, errno as c_int) }
 }
 
 pub fn ctx_set_debug(ctx: &Context, errno: i32) {
-    unsafe {
-        let &Context(ct) = ctx;
-        pj_ctx_set_debug(ct, errno as c_int)
-    }
+    let &Context(ct) = ctx;
+    unsafe { ffi::pj_ctx_set_debug(ct, errno as c_int) }
 }
 
 #[test]
@@ -161,9 +133,9 @@ fn transform_vec() {
     let ll_def = " +proj=latlong +ellps=clrk66";
     let p1 = init_plus(p1_def);
     let ll = init_plus(ll_def);
-    let mut x = [0.0f64, 1_000.0, 1_000_000.0];
-    let mut y = [0.0f64, 1_000.0, 1_000_000.0];
-    let result = transformv(&p1, &ll, &mut x, &mut y);
+    let x: [&mut f64, ..3] = [&mut 0.0f64, &mut 1_000.0, &mut 1_000_000.0];
+    let y: [&mut f64, ..3] = [&mut 0.0f64, &mut 1_000.0, &mut 1_000_000.0];
+    let result = transformv(&p1, &ll, &x, &y);
     assert_eq!(result, 0i32);
 }
 
